e778852af98fa524126ae7949e7a6104cfd0f6ea
Melmaphother <1559792558@qq.com>
2024-06-13 22:54:40 +0800

    add init-database

---
+++ src/sql_src/init_database.sql
@@ -0,0 +1,12 @@
+-- 在 bank 表中插入银行
+insert into bank(b_name, b_addr, b_phone)
+value ('东区银行', '东区', '10000000001');
+
+insert into bank(b_name, b_addr, b_phone)
+value ('西区银行', '西区', '10000000002');
+
+insert into bank(b_name, b_addr, b_phone)
+value ('中区银行', '中区', '10000000003');
+
+insert into bank(b_name, b_addr, b_phone)
+value ('高新区银行', '高新区', '10000000004');
