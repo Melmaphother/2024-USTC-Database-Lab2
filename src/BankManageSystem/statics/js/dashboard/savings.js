1e2b9b945112ee3b123624785f1dd08ba3aa52e4
Melmaphother <1559792558@qq.com>
2024-06-25 00:20:13 +0800

    modify some styles in sidebar

--- src/BankManageSystem/statics/js/dashboard/savings.js
+++ src/BankManageSystem/statics/js/dashboard/savings.js
@@ -1,3 +1,50 @@
+function openAddAccountModal() {
+    document.getElementById('addAccountModal').style.display = 'block';
+    document.getElementById('pageContent').style.filter = 'blur(5px)';
+}
+
+function closeAddAccountModal() {
+    document.getElementById('addAccountModal').style.display = 'none';
+    document.getElementById('pageContent').style.filter = 'none';
+}
+
+function openDepositModal(button) {
+    document.getElementById('hiddenDepositAccountNumber').value = button.getAttribute('data-deposit-account-number');
+    document.getElementById('depositModal').style.display = 'block';
+    document.getElementById('pageContent').style.filter = 'blur(5px)';
+}
+
+function closeDepositModal() {
+    document.getElementById('depositModal').style.display = 'none';
+    document.getElementById('pageContent').style.filter = 'none';
+}
+
+function openWithdrawModal(button) {
+    document.getElementById('hiddenWithdrawAccountNumber').value = button.getAttribute('data-withdraw-account-number');
+    document.getElementById('hiddenWithdrawBalance').value = button.getAttribute('data-withdraw-balance');
+    document.getElementById('hiddenWithdrawLimit').value = button.getAttribute('data-withdraw-limit');
+    document.getElementById('withdrawModal').style.display = 'block';
+    document.getElementById('pageContent').style.filter = 'blur(5px)';
+}
+
+function closeWithdrawModal() {
+    document.getElementById('withdrawModal').style.display = 'none';
+    document.getElementById('pageContent').style.filter = 'none';
+}
+
+function openTransferModal(button) {
+    document.getElementById('hiddenTransferAccountNumber').value = button.getAttribute('data-transfer-account-number');
+    document.getElementById('hiddenTransferBalance').value = button.getAttribute('data-transfer-balance');
+    document.getElementById('hiddenTransferLimit').value = button.getAttribute('data-transfer-limit');
+    document.getElementById('transferModal').style.display = 'block';
+    document.getElementById('pageContent').style.filter = 'blur(5px)';
+}
+
+function closeTransferModal() {
+    document.getElementById('transferModal').style.display = 'none';
+    document.getElementById('pageContent').style.filter = 'none';
+}
+
 function openDetailModal(button) {
     let account_number = button.getAttribute('data-detail-account-number')
     document.getElementById('hiddenDetailAccountNumber').textContent = account_number;
